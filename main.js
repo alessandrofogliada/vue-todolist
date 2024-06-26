b62cda43362b8e8a221ba1e87f5887bce483f24a
alessandrofogliada <fogliada.a@gmail.com>
2024-06-26 18:22:18 +0200

    1.0.3 Aggiunta la frase d'errore

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ createApp ({
     data(){
         return{
             newTask: '',
+            inputError: false,
             // creiamo un array di oggetti per la to do list 
             listToDo:[
 
@@ -32,14 +33,29 @@ createApp ({
             ]
         }
     },
-    methods: {
-        addTask() {
-            this.listToDo.unshift(this.newTask)
-        },
 
+    methods: {
+        // MILESTONE 3
+        // Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+        // aggiungo un elemento all'array 
+        addTask() {
+            if (this.newTask !== '' && this.newTask.lenght >=5
+            ) {
+                this.listToDo.unshift({text:this.newTask});
+                this.inputError = false;
+            } else {
+                this.inputError = true;
+            }
+          
+            this.newTask ='';
+        },
+        // MILESTONE 2
+        // Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
+        // rimuovo un elemento utilizzando l'item 
         removeTask(index){
             this.listToDo.splice(index, 1);
-        }
+        },
+        // Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
     },
 }).mount('#app')
 
@@ -48,10 +64,11 @@ createApp ({
 
 // Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 
-// MILESTONE 2
-// Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
 
-// MILESTONE 3
-// Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.Bonus:
+
+
+// Bonus:
 // 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+
+
 // 2- cliccando sul testo dell’item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)Buon lavoro e buon divertimento! :lino-banfi2:
